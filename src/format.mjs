3754696957da0eab0de751e7fd9c5bfbe0aac610
sverweij <sverweij@users.noreply.github.com>
2022-06-24 21:00:32 +0200

    fix(format): adds unit test

--- src/format.mjs
+++ src/format.mjs
@@ -5,7 +5,8 @@ const OUTPUT_TYPE_TO_FUNCTION = {
   regex: formatToRegex,
   json: formatToJSON,
 };
+const DEFAULT_OUTPUT_TYPE = "json";
 
 export default function format(pChanges, pOutputType) {
-  return OUTPUT_TYPE_TO_FUNCTION[pOutputType](pChanges);
+  return OUTPUT_TYPE_TO_FUNCTION[pOutputType || DEFAULT_OUTPUT_TYPE](pChanges);
 }
---
+++ src/format.spec.mjs
@@ -0,0 +1,14 @@
+import { deepEqual } from "node:assert";
+import format from "./format.mjs";
+
+describe("format", () => {
+  it("by default formats as json", () => {
+    deepEqual(format([]), "[]");
+  });
+  it("returns a regex when passed regex as a format", () => {
+    deepEqual(format([], "regex"), "^()$");
+  });
+  it("returns json when passed json as a format", () => {
+    deepEqual(format([], "json"), "[]");
+  });
+});
