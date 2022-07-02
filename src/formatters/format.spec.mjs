dcbcd280e89914da5f4a5567b9cb043863f48a12
sverweij <sverweij@users.noreply.github.com>
2022-07-02 17:08:23 +0200

    feature(format): adds a (plain) 'object' formatter and makes it the default (API) reporter
    the default reporter for the cli remains 'regex'

--- src/formatters/format.spec.mjs
+++ src/formatters/format.spec.mjs
@@ -2,8 +2,8 @@ import { deepEqual } from "node:assert";
 import format from "./format.mjs";
 
 describe("format", () => {
-  it("by default formats as json", () => {
-    deepEqual(format([]), "[]");
+  it("by default formats as a plain object", () => {
+    deepEqual(format([]), []);
   });
   it("returns a regex when passed regex as a format", () => {
     deepEqual(format([], "regex"), "^()$");
