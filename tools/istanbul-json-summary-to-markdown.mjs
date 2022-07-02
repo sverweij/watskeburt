f3a1c84b135427aa584c1d1d964ec14696bca16d
sverweij <sverweij@users.noreply.github.com>
2022-07-02 09:44:52 +0200

    ci: emit coverage summary in github step summary

---
+++ tools/istanbul-json-summary-to-markdown.mjs
@@ -0,0 +1,41 @@
+function formatMetric(pCoverageMetric) {
+  return `${pCoverageMetric.pct}%|${pCoverageMetric.covered}/${pCoverageMetric.total}|${pCoverageMetric.skipped}`;
+}
+
+function formatSummary(pCoverageSummary) {
+  return (
+    `|Metric|Coverage|Covered/Total|Skipped|\n` +
+    `|:--|--:|:--:|--:|\n` +
+    `|Statements|${formatMetric(pCoverageSummary.total.statements)}|\n` +
+    `|Branches|${formatMetric(pCoverageSummary.total.branches)}|\n` +
+    `|Functions|${formatMetric(pCoverageSummary.total.functions)}|\n` +
+    `|Lines|${formatMetric(pCoverageSummary.total.lines)}|\n`
+  );
+}
+/**
+ *
+ * @param {readStream} pStream stream whose characters are to be slapped between header and footer
+ * @param {writeStream} pOutStream stream to write to
+ */
+function wrap(pInStream, pOutStream) {
+  let lBuffer = "";
+
+  pInStream
+    .on("end", () => {
+      pOutStream.write(formatSummary(JSON.parse(lBuffer)));
+      pOutStream.end();
+    })
+    .on(
+      "error",
+      /* c8 ignore start */
+      (pError) => {
+        process.stderr.write(`${pError}\n`);
+      }
+      /* c8 ignore stop */
+    )
+    .on("data", (pChunk) => {
+      lBuffer += pChunk;
+    });
+}
+
+wrap(process.stdin, process.stdout);
