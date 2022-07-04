75d82953a4b0a91f2280aa78fb661f2755bacf61
sverweij <sverweij@users.noreply.github.com>
2022-07-04 22:39:10 +0200

    chore: makes the thing a bit more distribution ready

--- src/main.mjs
+++ src/main.mjs
@@ -5,6 +5,19 @@ import {
 import { getDiffLines, getStatusShort } from "./get-diff-lines.mjs";
 import format from "./formatters/format.mjs";
 
+/**
+ *
+ * returns a list of files changed since pOldThing. With pOptions
+ * you can
+ * - influence whether you want to have the output as an array of
+ *   IChange-s or one of the other output formats (outputType)
+ * - tell whether you want to take untracked files into account as
+ *   well (by setting trackedOnly to false)
+ *
+ * @param {string} pOldThing reference to a commit, branch, tag, ...
+ * @param {import("../types/diff-dat.js").IOptions} pOptions
+ * @returns {string|import("../types/diff-dat.js").IChange[]}
+ */
 export function convert(pOldThing, pOptions) {
   let lChanges = convertDiffLines(getDiffLines(pOldThing));
 
