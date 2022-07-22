25152c4eb0481159be6388cdd4f7381425e23852
Sander Verweij <sverweij@users.noreply.github.com>
2022-07-22 22:43:54 +0200

    chore: adds tsc check on types folder (#17)

--- .dependency-cruiser.js
+++ .dependency-cruiser.js
@@ -264,9 +264,9 @@ module.exports = {
        dependency-cruiser's current working directory). When not provided
        defaults to './tsconfig.json'.
      */
-    // tsConfig: {
-    //  fileName: './tsconfig.json'
-    // },
+    tsConfig: {
+      fileName: "./tsconfig.json",
+    },
 
     /* Webpack configuration to use to get resolve options from.
 
