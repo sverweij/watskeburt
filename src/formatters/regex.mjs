3ca0fff5a0614a0b41207b1c8e14ab52a78f9798
sverweij <sverweij@users.noreply.github.com>
2022-07-24 11:03:34 +0200

    refactor(format): use sets instead of arrays for filtering parameters
    the Set data structure is better suited for the task at hand + it's typically faster than arrays of the same size according to MDN

--- src/formatters/regex.mjs
+++ src/formatters/regex.mjs
@@ -1,37 +1,46 @@
 import { extname } from "node:path";
 
+const DEFAULT_EXTENSIONS = new Set([
+  ".cjs",
+  ".cjsx",
+  ".coffee",
+  ".csx",
+  ".js",
+  ".json",
+  ".jsx",
+  ".litcoffee",
+  ".ls",
+  ".mjs",
+  ".svelte",
+  ".ts",
+  ".tsx",
+  ".vue",
+  ".vuex",
+]);
+
+const DEFAULT_CHANGE_TYPES = new Set([
+  "modified",
+  "added",
+  "renamed",
+  "copied",
+  "untracked",
+]);
 /**
  *
  * @param {import('../types/watskeburt').IChange[]} pChanges
- * @param {string[]} pExtensions
- * @param {import('../types/watskeburt').changeTypeType[]} pChangeTypes
+ * @param {Set<string>} pExtensions
+ * @param {Set<import('../types/watskeburt').changeTypeType>} pChangeTypes
  * @return {string}
  */
 export default function formatToRegex(
   pChanges,
-  pExtensions = [
-    ".cjs",
-    ".cjsx",
-    ".coffee",
-    ".csx",
-    ".js",
-    ".json",
-    ".jsx",
-    ".litcoffee",
-    ".ls",
-    ".mjs",
-    ".svelte",
-    ".ts",
-    ".tsx",
-    ".vue",
-    ".vuex",
-  ],
-  pChangeTypes = ["modified", "added", "renamed", "copied", "untracked"]
+  pExtensions = DEFAULT_EXTENSIONS,
+  pChangeTypes = DEFAULT_CHANGE_TYPES
 ) {
   const lChanges = pChanges
-    .filter((pChange) => pChangeTypes.includes(pChange.changeType))
+    .filter((pChange) => pChangeTypes.has(pChange.changeType))
     .map(({ name }) => name)
-    .filter((pName) => pExtensions.includes(extname(pName)))
+    .filter((pName) => pExtensions.has(extname(pName)))
     // .replace(/\./g, "\\\\.")
     .join("|");
   return `^(${lChanges})$`;
