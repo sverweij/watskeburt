7aff51cef996aae4a84e54ffe4414c7fc9893eb6
sverweij <sverweij@users.noreply.github.com>
2023-04-29 18:38:17 +0200

    refactor: hides internal parameters for getSHA and getSHASync

--- dist/esm/main.js
+++ dist/esm/main.js
@@ -1,22 +1,13 @@
 import { convertDiffLines, convertStatusLines, } from "./convert-to-change-object.js";
-import { getDiffLinesSync, getSHASync, getStatusShortSync, } from "./git-primitives-sync.js";
+import * as primitivesSync from "./git-primitives-sync.js";
+import * as primitives from "./git-primitives.js";
 import format from "./formatters/format.js";
-import { getDiffLines, getStatusShort, getSHA } from "./git-primitives.js";
-export function listSync(pOldRevision, pNewRevision, pOptions) {
-    const lOldRevision = pOldRevision || getSHASync();
-    const lOptions = pOptions || {};
-    let lChanges = convertDiffLines(getDiffLinesSync(lOldRevision, pNewRevision));
-    if (!lOptions.trackedOnly) {
-        lChanges = lChanges.concat(convertStatusLines(getStatusShortSync()).filter(({ changeType }) => changeType === "untracked"));
-    }
-    return format(lChanges, lOptions.outputType);
-}
 export async function list(pOldRevision, pNewRevision, pOptions) {
-    const lOldRevision = pOldRevision || (await getSHA());
+    const lOldRevision = pOldRevision || (await primitives.getSHA());
     const lOptions = pOptions || {};
     const [lDiffLines, lStatusLines] = await Promise.all([
-        getDiffLines(lOldRevision, pNewRevision),
-        !lOptions.trackedOnly ? getStatusShort() : "",
+        primitives.getDiffLines(lOldRevision, pNewRevision),
+        !lOptions.trackedOnly ? primitives.getStatusShort() : "",
     ]);
     let lChanges = convertDiffLines(lDiffLines);
     if (!lOptions.trackedOnly) {
@@ -24,5 +15,18 @@ export async function list(pOldRevision, pNewRevision, pOptions) {
     }
     return format(lChanges, lOptions.outputType);
 }
-export { getSHASync } from "./git-primitives-sync.js";
-export { getSHA } from "./git-primitives.js";
+export function listSync(pOldRevision, pNewRevision, pOptions) {
+    const lOldRevision = pOldRevision || primitivesSync.getSHASync();
+    const lOptions = pOptions || {};
+    let lChanges = convertDiffLines(primitivesSync.getDiffLinesSync(lOldRevision, pNewRevision));
+    if (!lOptions.trackedOnly) {
+        lChanges = lChanges.concat(convertStatusLines(primitivesSync.getStatusShortSync()).filter(({ changeType }) => changeType === "untracked"));
+    }
+    return format(lChanges, lOptions.outputType);
+}
+export function getSHA() {
+    return primitives.getSHA();
+}
+export function getSHASync() {
+    return primitivesSync.getSHASync();
+}
--- src/main.ts
+++ src/main.ts
@@ -3,47 +3,23 @@ import {
   convertDiffLines,
   convertStatusLines,
 } from "./convert-to-change-object.js";
-import {
-  getDiffLinesSync,
-  getSHASync,
-  getStatusShortSync,
-} from "./git-primitives-sync.js";
+import * as primitivesSync from "./git-primitives-sync.js";
+import * as primitives from "./git-primitives.js";
 import format from "./formatters/format.js";
-import { getDiffLines, getStatusShort, getSHA } from "./git-primitives.js";
-
-export function listSync(
-  pOldRevision?: string,
-  pNewRevision?: string,
-  pOptions?: IOptions
-): IChange[] | string {
-  const lOldRevision: string = pOldRevision || getSHASync();
-  const lOptions: IOptions = pOptions || {};
-
-  let lChanges = convertDiffLines(getDiffLinesSync(lOldRevision, pNewRevision));
-
-  if (!lOptions.trackedOnly) {
-    lChanges = lChanges.concat(
-      convertStatusLines(getStatusShortSync()).filter(
-        ({ changeType }) => changeType === "untracked"
-      )
-    );
-  }
-  return format(lChanges, lOptions.outputType);
-}
 
 export async function list(
   pOldRevision?: string,
   pNewRevision?: string,
   pOptions?: IOptions
 ): Promise<IChange[] | string> {
-  const lOldRevision: string = pOldRevision || (await getSHA());
+  const lOldRevision: string = pOldRevision || (await primitives.getSHA());
   const lOptions: IOptions = pOptions || {};
 
   const [lDiffLines, lStatusLines] = await Promise.all([
-    getDiffLines(lOldRevision, pNewRevision),
-    // to stay consistent with the use of trackedOnly below
+    primitives.getDiffLines(lOldRevision, pNewRevision),
+    // to stay consistent with the use of trackedOnly below: negated condition
     // eslint-disable-next-line no-negated-condition
-    !lOptions.trackedOnly ? getStatusShort() : "",
+    !lOptions.trackedOnly ? primitives.getStatusShort() : "",
   ]);
 
   let lChanges = convertDiffLines(lDiffLines);
@@ -58,5 +34,37 @@ export async function list(
   return format(lChanges, lOptions.outputType);
 }
 
-export { getSHASync } from "./git-primitives-sync.js";
-export { getSHA } from "./git-primitives.js";
+export function listSync(
+  pOldRevision?: string,
+  pNewRevision?: string,
+  pOptions?: IOptions
+): IChange[] | string {
+  const lOldRevision: string = pOldRevision || primitivesSync.getSHASync();
+  const lOptions: IOptions = pOptions || {};
+
+  let lChanges = convertDiffLines(
+    primitivesSync.getDiffLinesSync(lOldRevision, pNewRevision)
+  );
+
+  if (!lOptions.trackedOnly) {
+    lChanges = lChanges.concat(
+      convertStatusLines(primitivesSync.getStatusShortSync()).filter(
+        ({ changeType }) => changeType === "untracked"
+      )
+    );
+  }
+  return format(lChanges, lOptions.outputType);
+}
+
+// Although it looks like getSHA and getSHASync could be re-exported e.g.
+//   export { getSHA } from "./git-primitives.js"
+// the 'primitives' have a extra parameter we don't want to expose,
+// so instead we wrap them:
+
+export function getSHA(): Promise<string> {
+  return primitives.getSHA();
+}
+
+export function getSHASync(): string {
+  return primitivesSync.getSHASync();
+}
