82691002e22ad4e165f8d60022ffcab9541a122e
sverweij <sverweij@users.noreply.github.com>
2024-07-05 21:19:42 +0200

    refactor(format regex): come to think of it, we can alsu remove the default extensions set

--- dist/format/regex.js
+++ dist/format/regex.js
@@ -1,5 +1,4 @@
 import { extname } from "node:path";
-const DEFAULT_EXTENSIONS = new Set([]);
 const DEFAULT_CHANGE_TYPES = new Set([
 	"modified",
 	"added",
@@ -9,7 +8,7 @@ const DEFAULT_CHANGE_TYPES = new Set([
 ]);
 export default function formatAsRegex(
 	pChanges,
-	pExtensions = DEFAULT_EXTENSIONS,
+	pExtensions,
 	pChangeTypes = DEFAULT_CHANGE_TYPES,
 ) {
 	const lChanges = pChanges
