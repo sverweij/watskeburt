3d834633e4994c71a7a38431c61b9eb17f713cbe
Sander Verweij <sverweij@users.noreply.github.com>
2024-12-03 13:53:53 +0100

    fix(format): defaults 'extensions' to '*' (#52)
    ## Description
    
    - when in 'format' _extensions_ aren't passed, instead of throwing a
    TypeError, default to _all extensions_
    
    ## Motivation and Context
    
    Addresses an issue raised in #49 
    
    ## How Has This Been Tested?
    
    - [x] green ci
    - [x] additional automated non-regression tests
    
    ## Types of changes
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] Documentation only change
    - [ ] Refactor (non-breaking change which fixes an issue without
    changing functionality)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)

--- src/format/format.spec.ts
+++ src/format/format.spec.ts
@@ -13,4 +13,57 @@ describe("format", () => {
   it("returns json when passed json as a format", () => {
     deepEqual(format([], "json", ""), "[]");
   });
+  it("doesn't throw a TypeError when 'extensions' aren't provided", () => {
+    // @ts-expect-error 2554 - Expected 3 arguments, but got 2 is a correct
+    // error, but we want to test the behavior of the function
+    // when it's not passed at all, which is very possible in javascript
+    // and (with a ts-expect-error comment) in typescript as well.
+    deepEqual(format([], "regex"), "^()$");
+  });
+  it("ensures regexp doesn't filter if no extensions are provided", () => {
+    deepEqual(
+      // @ts-expect-error 2554 - see above
+      format(
+        [
+          {
+            type: "modified",
+            name: "dist/format/format.js",
+          },
+          {
+            type: "modified",
+            name: "src/format/format.spec.ts",
+          },
+          {
+            type: "modified",
+            name: "src/format/format.ts",
+          },
+        ],
+        "regex",
+      ),
+      "^(dist/format/format[.]js|src/format/format[.]spec[.]ts|src/format/format[.]ts)$",
+    );
+  });
+  it("ensures regexp _does_ filter when provided with the empty string as list of extensions)", () => {
+    deepEqual(
+      format(
+        [
+          {
+            type: "modified",
+            name: "dist/format/format.js",
+          },
+          {
+            type: "modified",
+            name: "src/format/format.spec.ts",
+          },
+          {
+            type: "modified",
+            name: "src/format/format.ts",
+          },
+        ],
+        "regex",
+        "",
+      ),
+      "^()$",
+    );
+  });
 });
